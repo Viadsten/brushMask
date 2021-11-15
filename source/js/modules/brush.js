9b826ec0fc4cbc25cd7081a0c510616bb08deaa9
viadsten <stanulevich.vg@yandex.ru>
2021-11-15 23:20:27 +0300

    рефакторит bootBrush

--- source/js/modules/brush.js
+++ source/js/modules/brush.js
@@ -1,7 +1,7 @@
 class Brush {
   constructor(node, config) {
     this.node = node;
-    this.path = this._getPath();
+    this.path = this.node.querySelector('#brush-mask path');
     this.pathLength = this.path.getTotalLength();
     this.time = config.time;
     this.ease = config.ease;
@@ -31,14 +31,14 @@ class Brush {
     this.path.style.strokeDasharray = this.pathLength;
     this.path.style.strokeDashoffset = this.pathLength;
   }
-
-  _getPath() {
-    return this.node.querySelector('#brush-mask path');
-  }
 }
 
 const initBrush = () => {
   const brushImage = document.querySelector('.brush');
+
+  if (!brushImage) {
+    return;
+  }
 
   const bootBrush = new Brush(brushImage, {
     time: 2.7,
